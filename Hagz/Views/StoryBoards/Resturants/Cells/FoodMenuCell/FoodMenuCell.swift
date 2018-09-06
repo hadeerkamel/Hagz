a17e70af488eba27ec41ce2bbeb3f218f62a2599
Hadeer Kamel <hadeer@hadeers-mac.local>
2018-09-06 23:39:19 +0200

    Design Finished

---
+++ Hagz/Views/StoryBoards/Resturants/Cells/FoodMenuCell/FoodMenuCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodMenuCell.swift
+//  Hagz
+//
+//  Created by Hadeer Kamel on 9/6/18.
+//  Copyright © 2018 Hadeer Kamel. All rights reserved.
+//
+
+import UIKit
+
+class FoodMenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
